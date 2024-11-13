83e736d5bf923b7448c53b3dbca24eb2a863024f
solmsl <mejiasilesol@gmail.com>
2024-11-13 19:44:53 -0300

    crear rescatista

--- src/admins/scripts/altaR.js
+++ src/admins/scripts/altaR.js
@@ -8,8 +8,6 @@ function verificar(){
         window.location.href = 'https://riab-project.vercel.app';
         return;
     }
-
-    traerDatos();
 }
 
 //Decodifica el token y toma el sector donde se encuentra el DNI
@@ -24,32 +22,53 @@ const getDniFromToken = (token) => {
         return null;
     }
 };
-
-async function traerDatos() {
-    try {
-      //luego cambiar el localhost a riab-api.vercel.app, por el momento dejarlo asi
-        const response = await fetch(`${app}/rescatistas/${getDniFromToken(token)}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-                // "Access-Control-Allow-Origin": "https://localhost:5500",
-                "Access-Control-Allow-Credentials": true,
+const dni=document.getElementById("dni");
+const nombre=document.getElementById("nombre");
+const apellido=document.getElementById("apellido");
+const telefono=document.getElementById("telefono");
+const direccion=document.getElementById("direccion");
+const genero=document.getElementById("genero");
+const email=document.getElementById("email");
+const passw=document.getElementById("passw");
+const re_passw=document.getElementById("re_passw");
+const form={
+    dni,
+    nombre,
+    apellido,
+    telefono,
+    direccion,
+    genero,
+    email,
+    passw,
+    re_passw
+};
+document.getElementById("formulario").addEventListener((e)=>{
+    async function crearRescatista() {
+        try {
+          //luego cambiar el localhost a riab-api.vercel.app, por el momento dejarlo asi
+            const response = await fetch(`${app}/rescatistas/registro`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    // "Access-Control-Allow-Origin": "https://localhost:5500",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body:JSON.stringify(form)
+            });
+    
+            const data = await response.json();
+    
+            if (response.ok && data.success) {
+                console.log(data.data);
+            } else {
+                alert('Error: ' + data.error);
+                throw new Error(data.error || 'Error desconocido en el servidor.');
             }
-        });
-
-        const data = await response.json();
-
-        if (response.ok && data.success) {
-            console.log(data.data);
-        } else {
-            alert('Error: ' + data.error);
-            throw new Error(data.error || 'Error desconocido en el servidor.');
+    
+        } catch (error) {
+            console.error('Error: ', error);
         }
-
-    } catch (error) {
-        console.error('Error: ', error);
     }
-}
-  
+}) 
 verificar();

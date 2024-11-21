f15fb018ba83556ac439019eeeaba41e2cda53b1
nitram <164651909+lenrit@users.noreply.github.com>
2024-11-20 22:55:39 -0300

    Add files via upload

--- src/admins/scripts/bajaR.js
+++ src/admins/scripts/bajaR.js
@@ -1,3 +1,9 @@
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+const token=getCookie("userInfo");
 const app = "https://riab-api.vercel.app";
 document.getElementById('formulario').addEventListener("submit",async (e)=>{
     e.preventDefault();

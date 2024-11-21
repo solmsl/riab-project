7a1c00dcb745010b42ec9fe02344aaf4711b28d2
MicaelaMejiaVargas <mejiavargasmicaela@gmail.com>
2024-11-21 00:04:44 -0300

    eliminando linea de mas

--- src/soloRescatistas/script-rescatistas/altamascotas.js
+++ src/soloRescatistas/script-rescatistas/altamascotas.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('formulario');
   const especieSelect = document.getElementById('especie');
   const razaSelect = document.getElementById('raza');
-  especieSelect.innerHTML = '<option value="">Seleccionar Especie</option>';
+
   // Mapeo de especies a razas
   const razasPorEspecie = {
     perro: ['Labrador', 'Bulldog', 'Poodle', 'Golden Retriever','Caniche','Pekines','Salchicha', 'Bulldog Frances', 'Pitbull', 'Chihuahua', 'Pastor Aleman', 'No identifico', 'Otro'],

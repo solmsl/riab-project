80c77b71f0ab5813be1c1178b3a7564a6a99e324
nitram <164651909+lenrit@users.noreply.github.com>
2024-11-20 22:08:50 -0300

    Add files via upload

---
+++ src/admins/scripts/bajaR.js
@@ -0,0 +1,30 @@
+const app = "https://riab-api.vercel.app";
+document.getElementById('formulario').addEventListener("submit",async (e)=>{
+    e.preventDefault();
+    const dni=document.getElementById("dni").value;
+    try {
+        //luego cambiar el localhost a riab-api.vercel.app, por el momento dejarlo asi
+        const response = await fetch(`${app}/${dni}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Authorization': `Bearer ${token}`,
+                "Access-Control-Allow-Origin": "https://riab-api.vercel.app",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body:JSON.stringify(form)
+        });
+
+        const data = await response.json();
+
+        if (response.ok && data.success) {
+            console.log(data.data);
+        } else {
+            alert('Error: ' + data.error);
+            throw new Error(data.error || 'Error desconocido en el servidor.');
+        }
+    
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+})
